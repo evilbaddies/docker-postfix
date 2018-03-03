441b3fa982cd086dfb439fba91dd1cb7e2d315f1
erron <erronsilver@gmail.com>
2018-03-03 20:32:31 +0200

    Initial commit

---
+++ conf/postfix/postfix_setup_postconf.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -ex
+
+postconf -e "myhostname = $MAIL_FQDN"
+postconf -e "mydomain = $MAIL_DOMAIN"
+postconf -e "mydestination = $MAIL_FQDN, $MAIL_DOMAIN, localhost, localhost.localdomain"
+postconf -e 'mail_spool_directory=/var/spool/mail/'
+postconf -e 'mailbox_command='
+
+postconf -e 'virtual_mailbox_domains = proxy:mysql:/etc/postfix/sql/mysql_virtual_domains_maps.cf'
+postconf -e 'virtual_alias_maps = proxy:mysql:/etc/postfix/sql/mysql_virtual_alias_maps.cf, proxy:mysql:/etc/postfix/sql/mysql_virtual_alias_domain_maps.cf, proxy:mysql:/etc/postfix/sql/mysql_virtual_alias_domain_catchall_maps.cf'
+postconf -e 'virtual_mailbox_maps = proxy:mysql:/etc/postfix/sql/mysql_virtual_mailbox_maps.cf, proxy:mysql:/etc/postfix/sql/mysql_virtual_alias_domain_mailbox_maps.cf'
+postconf -e 'virtual_mailbox_base = /var/mail/vmail'
+postconf -e 'virtual_uid_maps = 5000'
+postconf -e 'virtual_gid_maps = 5000'
+postconf -e 'message_size_limit = 30720000'
+
+postconf -e 'smtpd_sasl_auth_enable = yes'
+postconf -e 'broken_sasl_auth_clients = yes'
+postconf -e 'smtpd_sasl_authenticated_header = yes'
+postconf -e 'smtpd_sasl_type = dovecot'
+postconf -e 'smtpd_sasl_path = private/auth'
+postconf -e 'smtpd_recipient_restrictions = permit_mynetworks, permit_sasl_authenticated, reject_unauth_destination'
+postconf -e 'smtpd_use_tls = yes'
+postconf -e 'smtpd_tls_cert_file = /etc/postfix/tls/tls.crt'
+postconf -e 'smtpd_tls_key_file = /etc/postfix/tls/tls.key'
+postconf -e 'smtpd_tls_security_level = may'
+
+postconf -e 'smtpd_upstream_proxy_protocol = haproxy'
---
+++ run-postfix.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+set -ex
+
+groupadd -g 5000 vmail || true
+useradd -g vmail -u 5000 -d /var/mail/vmail -m vmail || true
+chown -R vmail.vmail /var/mail/vmail
+mkdir -p /var/spool/postfix/var/run/saslauthd || true
+
+####################################################################
+# Postfix stuff
+####################################################################
+# Need to copy these for so that postfix will find them becuase of chroot
+cp -f /etc/services /var/spool/postfix/etc/
+cp -f /etc/resolv.conf /var/spool/postfix/etc/
+
+# Enable port 587 by uncommenting line in master.cf
+sed -i '/submission.*inet.*smtpd/s/^#//g' /etc/postfix/master.cf
+
+# Set the FQDN for mail server in /etc/mailname
+sed -i 's/mail.replace.me/$MAIL_FQDN/g' /etc/mailname
+
+/opt/postfix/conf/postfix/postfix_setup_mysql.sh
+/opt/postfix/conf/postfix/postfix_setup_postconf.sh
+
+####################################################################
+# SASL stuff
+####################################################################
+cp -f /opt/postfix/conf/sasl/saslauthd /etc/default/
+
+sed -e "s/\$POSTFIX_DB_HOST/$POSTFIX_DB_HOST/;
+        s/\$POSTFIX_DB_NAME/$POSTFIX_DB_NAME/; 
+        s/\$POSTFIX_DB_USER/$POSTFIX_DB_USER/;
+        s/\$POSTFIX_DB_PASSWORD/$POSTFIX_DB_PASSWORD/" < /opt/postfix/conf/sasl/smtpd.conf.tmpl > /etc/postfix/sasl/smtpd.conf
+
+sed -e "s/\$POSTFIX_DB_HOST/$POSTFIX_DB_HOST/;
+        s/\$POSTFIX_DB_NAME/$POSTFIX_DB_NAME/;
+        s/\$POSTFIX_DB_USER/$POSTFIX_DB_USER/;
+        s/\$POSTFIX_DB_PASSWORD/$POSTFIX_DB_PASSWORD/" < /opt/postfix/conf/sasl/pam.d-smtp.tmpl > /etc/pam.d/smtp
+
+
+####################################################################
+# Dovecot stuff
+####################################################################
+
+cp -f /opt/postfix/conf/dovecot/dovecot.conf /etc/dovecot/
+
+sed -e "s/\$POSTFIX_DB_HOST/$POSTFIX_DB_HOST/;
+        s/\$POSTFIX_DB_NAME/$POSTFIX_DB_NAME/;
+        s/\$POSTFIX_DB_USER/$POSTFIX_DB_USER/;
+        s/\$POSTFIX_DB_PASSWORD/$POSTFIX_DB_PASSWORD/" < /opt/postfix/conf/dovecot/dovecot-sql.conf.tmpl > /etc/dovecot/dovecot-sql.conf
+
+service rsyslog restart
+service saslauthd restart
+service dovecot restart
+
+postfix_status=`postfix status || true`
+if [[ -z "${postfix_status##*$is running*}" ]]; then
+    postfix stop
+fi 
+
+postfix start
+
+tail -f /var/log/mail.log
